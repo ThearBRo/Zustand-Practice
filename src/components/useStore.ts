2d08222b4d70b361d687e341d24dd663b3cadb51
Thearith <sornviraksothearith@gmail.com>
2025-05-30 07:38:25 +0700

    Update useStore.ts

--- src/components/useStore.ts
+++ src/components/useStore.ts
@@ -97,3 +97,27 @@ export const useTipStore = create<TipStore>((set) => ({
 
 //==== Creating Advice app ====//
 
+
+//==== This is for expense tracker ====//
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+}
+
+interface ExpenseStore {
+  expenses: Expense[];
+  addExpense: (expense: Expense) => void;
+  removeExpense: (id: number) => void;
+}
+
+export const useExpenseStore = create<ExpenseStore>((set) => ({
+  expenses: [],
+  addExpense: (expense) =>
+    set((state) => ({ expenses: [...state.expenses, expense] })),
+
+  removeExpense: (id) =>
+    set((state) => ({
+      expenses: state.expenses.filter((expense) => expense.id != id),
+    })),
+}));
